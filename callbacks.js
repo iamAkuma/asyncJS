b2f8de7f389c9fe2959fe6ba81718f0e9bbe95be
Saral Bajracharya <rmanbajra@gmail.com>
2023-05-18 18:09:33 +0545

    Async JS

---
+++ callbacks.js
@@ -0,0 +1,16 @@
+const posts = [
+    {title: 'Post1', body: 'This is post1'},
+    {title: 'Post2', body: 'This is post2'}
+];
+
+function getPosts() {
+    setTimeout(()=>{
+        let output = '';
+        posts.forEach((post, index)=>{
+            output += `<li>${post.title}</li>`;
+        });
+        document.body.innerHTML = output;
+    }, 1000);
+}
+
+getPosts();
