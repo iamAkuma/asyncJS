13d58e11edf5289f5266d3f9867782b20d5af1fd
Saral Bajracharya <rmanbajra@gmail.com>
2023-05-20 19:40:59 +0545

    Callbacks Used

--- callbacks.js
+++ callbacks.js
@@ -1,23 +1,32 @@
-const posts = [
-    {title: 'Post1', body: 'This is post1'},
-    {title: 'Post2', body: 'This is post2'}
-];
+// const posts = [
+//     {title: 'Post1', body: 'This is post1'},
+//     {title: 'Post2', body: 'This is post2'}
+// ];
 
-function getPosts() {
-    setTimeout(()=>{
-        let output = '';
-        posts.forEach((post, index)=>{
-            output += `<li>${post.title}</li>`;
-        });
-        document.body.innerHTML = output;
-    }, 1000);
-}
-function createPosts(post, callback){
-    setTimeout(()=>{
-        posts.push(post);
-        callback();
-    },2000);
-}
-// getPosts();
+// function getPosts() {
+//     setTimeout(()=>{
+//         let output = '';
+//         posts.forEach((post, index)=>{
+//             output += `<li>${post.title}</li>`;
+//         });
+//         document.body.innerHTML = output;
+//     }, 1000);
+// }
+// function createPosts(post, callback){
+//     setTimeout(()=>{
+//         posts.push(post);
+//         callback();
+//     },2000);
+// }
+// // getPosts();
 
-createPosts({title: 'Post3', body:'This is post3'}, getPosts);
+// createPosts({title: 'Post3', body:'This is post3'}, getPosts);
+
+// A function that simulates an asynchronous operation
+function fetchData(callback) {
+    setTimeout(function() {
+      const data = "Hello, world!";
+      callback(data); // Invoke the callback function with the data
+    }, 2000); // Simulate a delay of 2 seconds
+  }
+  
